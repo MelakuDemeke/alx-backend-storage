5e0395055baa874f6715869154a2c5c13fe3e88e
Melaku <MelakuDemeke>
2023-10-12 17:05:31 +0300

    shorten the comment

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
--- Retrieve and rank the countries of origin for metal bands based on
---their total number of fans.
-SELECT origin, SUM(fans) AS nb_fans
+-- Rank bands' origins by total non-unique fans.
+SELECT origin AS origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
