734be48179be2f7249d80f572cbb551e0a85f2cb
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 12:23:57 +0300

    connect to mongo and find  nginx logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
+from pymongo import MongoClient
+
 if __name__ == "__main__":
-    pass
+    client = MongoClient('mongodb://127.0.0.0:27017')
+    nginx_logs_collection = client.logs.nginx
