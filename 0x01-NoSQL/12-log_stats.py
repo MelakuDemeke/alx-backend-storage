28ab20cc6908072f00475820da59fa29d895fb93
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 12:25:05 +0300

    find total logs count

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,3 +5,6 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.0:27017')
     nginx_logs_collection = client.logs.nginx
+
+    total_logs_count = nginx_logs_collection.count_documents({})
+    print(f'{total_logs_count} logs')
