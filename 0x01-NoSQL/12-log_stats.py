4037e4b352f599040d357191be4bb21051632ebc
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 12:41:09 +0300

    change ip and spell

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,21 +10,20 @@ if __name__ == "__main__":
     This is the main part of the script where it connects to a MongoDB database
     queries an nginx logs collection, and prints various statistics.
     '''
-    client = MongoClient('mongodb://127.0.0.0:27017')
+    client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_logs_collection = client.logs.nginx
 
     total_logs_count = nginx_logs_collection.count_documents({})
     print(f'{total_logs_count} logs')
 
     http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
+    print('methods:')
     for method in http_methods:
         method_count = nginx_logs_collection.count_documents({"method": method})
         print(f'\tmethod {method}: {method_count}')
-    
+
     status_check_count = nginx_logs_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
+
     print(f'{status_check_count} status check')
-
-
