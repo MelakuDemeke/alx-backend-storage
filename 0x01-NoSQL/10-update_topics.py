ec1191553fe698b0ba12497d883ed431ef242559
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 11:49:51 +0300

    pycode style fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 '''A script for updating topics in a MongoDB collection.'''
+
+
 def update_topics(mongo_collection, name, topics):
     '''
     Update topics in the given MongoDB collection for a specific document.
