67ecdd82b41277797d8d612b5c20eeaf6f7543e7
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 11:45:50 +0300

    add doc string

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
-
+'''A script for updating topics in a MongoDB collection.'''
 def update_topics(mongo_collection, name, topics):
+    '''
+    Update topics in the given MongoDB collection for a specific document.
+
+    Parameters:
+    - mongo_collection: The MongoDB collection where the update performed
+    - name: The name of the document to be updated.
+    - topics: The new topics to be set in the document.
+
+    Returns:
+    None
+    '''
     mongo_collection.update_many(
         {name: name},
         {"$set": {"topics": topics}}
