9482e1c143a5f9008ec06dbe3269b8664faef56a
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 12:57:01 +0300

    fix the usage style

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,22 +5,23 @@ and prints various statistics.
 '''
 from pymongo import MongoClient
 
-def log_stats(mongo_collection, option=None):
-    total_logs_count = nginx_logs_coll.count_documents({})
-    print(f'{total_logs_count} logs')
+def log_stats(nginx_logs_coll, option=None):
+    items = {}
+    if option:
+        value = nginx_logs_coll.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return items
 
     http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('methods:')
+
+    total_logs_count = nginx_logs_coll.count_documents(items)
+    print(f"{total_logs_count} logs")
+    print("Methods:")
     for method in http_methods:
-        method_count = nginx_logs_coll.count_documents({"method": method})
-        print(f'\tmethod {method}: {method_count}')
-
-    status_check_count = nginx_logs_coll.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
-    print(f'{status_check_count} status check')
-
+        log_stats(nginx_logs_coll, method)
+        status_check = nginx_logs_coll.count_documents({"path": "/status"})
+        print(f"{status_check} status check")
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
