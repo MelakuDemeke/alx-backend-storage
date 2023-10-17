1d9f4c2f1359d8d4a885996992a3870ae2754196
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 13:17:57 +0300

    find top student

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def top_students(mongo_collection):
-    pass
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
