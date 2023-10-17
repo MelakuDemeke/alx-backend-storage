83462fb47a2a9b0c082f4a19cc498a4336c2d725
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 13:20:37 +0300

    add doc string

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+'''
+A function to find and return the top students from a MongoDB collection
+based on their average scores in specific topics.
+'''
+
 
 def top_students(mongo_collection):
+    '''
+    This function takes a MongoDB collection as input and returns
+    a cursor to the top students based on their average scores.
+    '''
     top_student = mongo_collection.aggregate([
         {
             "$project": {
