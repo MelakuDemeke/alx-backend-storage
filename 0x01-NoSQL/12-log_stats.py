2efad6f5756bf1e7e4d92006f130a2f3290989f9
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 12:52:33 +0300

    call method on main

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,16 +6,6 @@ and prints various statistics.
 from pymongo import MongoClient
 
 def log_stats(mongo_collection, option=None):
-    pass
-
-if __name__ == "__main__":
-    '''
-    This is the main part of the script where it connects to a MongoDB database
-    queries an nginx logs collection, and prints various statistics.
-    '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs_coll = client.logs.nginx
-
     total_logs_count = nginx_logs_coll.count_documents({})
     print(f'{total_logs_count} logs')
 
@@ -30,3 +20,10 @@ if __name__ == "__main__":
     )
 
     print(f'{status_check_count} status check')
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_logs_coll = client.logs.nginx
+    log_stats(nginx_logs_coll)
+    
