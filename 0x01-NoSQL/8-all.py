3658dae3b03095000bcfdf9b12cbdd91b3e201b7
Melaku Demeke <melaku.demeke789@gmail.com>
2023-10-17 11:50:39 +0300

    pycode fix

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 '''A script for listing all documents in a MongoDB collection.'''
+
+
 def list_all(mongo_collection):
     '''
     Lists all documents in the given MongoDB collection.
