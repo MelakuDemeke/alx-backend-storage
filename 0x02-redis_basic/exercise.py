d168a4226d029b304f1dc15d7a1fa79b893f2b92
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-18 15:33:34 +0300

    type anotiate the function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import redis
+from typing import Union
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
     
-    def store(self, data):
+    def store(self, data: Union[str, bytes, float, int]) -> str:
         pass
