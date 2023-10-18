a0bbe9dd1fe8513427af83ed2e4485f7dee3ce5c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-18 15:34:52 +0300

    return random key from the store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import redis
 from typing import Union
+from uuid import uuid4
 
 class Cache:
     def __init__(self):
@@ -8,4 +9,6 @@ class Cache:
         self._redis.flushdb()
     
     def store(self, data: Union[str, bytes, float, int]) -> str:
-        pass
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+        return random_key
