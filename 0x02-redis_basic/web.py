c1ab632260ecb018019d930d7bffcc1fa9c7a58b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-18 17:01:21 +0300

    return the wrapper

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,13 +2,22 @@
 import requests
 from typing import Callable
 from functools import wraps
+import redis
 
 
 def count_req(method: Callable) -> Callable:
+    redis = redis.Redis()
 
     @wraps(method)
     def wrapper(url):
-        pass
+        redis.incr(f"count:{url}")
+        cached_html = redis.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis.setex(f"cached:{url}", 10, html)
+        return html
+    return wrapper
 
 
 def get_page(url: str) -> str:
