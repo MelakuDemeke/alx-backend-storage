76e1c93f9f0efb51ba33307665cb6e422e7fbe75
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-18 15:35:42 +0300

    pycodestyle fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,11 +3,12 @@ import redis
 from typing import Union
 from uuid import uuid4
 
+
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: Union[str, bytes, float, int]) -> str:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
