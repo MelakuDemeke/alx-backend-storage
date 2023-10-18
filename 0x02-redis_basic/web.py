2eb1461e4d98d37a2d165a0faeb976f8c3a4f02f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-18 17:08:57 +0300

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,56 +1,32 @@
 #!/usr/bin/env python3
-import requests
-from typing import Callable
+""" Redis Module """
+
 from functools import wraps
 import redis
+import requests
+from typing import Callable
 
-redis = redis.Redis()
+redis_ = redis.Redis()
 
-def count_req(method: Callable) -> Callable:
-    """
-    A decorator function to count the number of requests made to a given URL
-    and cache the response using Redis.
 
-    Args:
-        method (Callable): A function that takes a URL and returns its
-        content as a string.
-
-    Returns:
-        Callable: A decorated function that count requests and caches responses
-    """
-
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url):
-        """
-        Wrapper function that decorates the input method.
-
-        Args:
-            url (str): The URL to fetch content from.
-
-        Returns:
-            str: The content of the URL as a string.
-        """
-        redis.incr(f"count:{url}")
-        cached_html = redis.get(f"cached:{url}")
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
         html = method(url)
-        redis.setex(f"cached:{url}", 10, html)
+        redis_.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
 
-@count_req
+
+@count_requests
 def get_page(url: str) -> str:
-    """
-    Retrieve the content of a given URL using the requests library.
-
-    Args:
-        url (str): The URL to fetch content from.
-
-    Returns:
-        str: The content of the URL as a string.
-    """
+    """ Obtain the HTML content of a  URL """
     req = requests.get(url)
-    req_text = req.text
-    return req_text
+    return req.text
