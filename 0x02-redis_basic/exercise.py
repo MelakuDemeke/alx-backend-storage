5ad51c254cf8d4c2c98463a9481ec144b9d049bb
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-18 16:04:10 +0300

    pycode style fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,6 +13,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
@@ -23,12 +24,12 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
+
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-            val = self._redis.get(key)
-            if fn:
-                val = fn(val)
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
             return val
 
     def get_str(self, key: str) -> str:
