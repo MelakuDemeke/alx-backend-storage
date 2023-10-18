273e09771a16f01cef3d209bba61b4eb8e915e60
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-18 16:56:41 +0300

    define requ counter

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 import requests
+from typing import Callable
+
+
+def count_req(method: Callable) -> Callable:
+    pass
+
 
 def get_page(url: str) -> str:
     req = requests.get(url)
