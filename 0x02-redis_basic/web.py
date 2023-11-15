f6f7f98ffbbf1bc51e8d9dccedc06c4063d16a8b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-15 15:42:00 +0300

    Add Redis caching and tracking to web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python3
-import requests
-from typing import Callable
-from functools import wraps
+'''A module with tools for request caching and tracking.
+'''
 import redis
+import requests
+from functools import wraps
+from typing import Callable
 
-redis_ = redis.Redis()
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
 def count_requests(method: Callable) -> Callable:
     """A decorator function to count the number of requests made to a given URL
     and cache the response using Redis.
     """
-    def wrapper(url):
+    @wraps(method)
+    def invoker(url) -> str:
         """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
 @count_requests
 def get_page(url: str) -> str:
     """Retrieve the content of a given URL using the requests library.
     """
-    req = requests.get(url)
-    return req.text
+    return requests.get(url).text
